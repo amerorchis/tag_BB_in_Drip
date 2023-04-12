45d2e6b1c127e96a61c6a45bf6a905cd6b84b438
amerorchis <andrew.p.smith95@gmail.com>
2023-04-11 18:45:08 -0600

    Chunk lists over 25 items

---
+++ api/chunk.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def chunk_names(names, email, batch):
+    chunks = [names[i:i+25] for i in range(0, len(names), 25)]
+    
+    # Make a POST request for each chunk
+    for i, chunk in enumerate(chunks):
+        batch += f" ({i+1} of {len(chunks)})"
+        payload = {"data": chunk, "batch": batch, "email":email}
+        r = requests.post("https://tag-constit.vercel.app", data=json.dumps(payload))
+    
+    return "All chunks posted successfully"
--- api/main_process.py
+++ api/main_process.py
@@ -1,24 +1,32 @@
 from api.modules import *
 from api.email_confirm import send_email
+from api.chunk import chunk_names
 
 # main workflow
 def main(data):
     try:
         notification_email = ''
         batch = ''
+        
         names, notification_email, batch = store_names(data)
-        emails, errors = blackbaud(names)
+
+        if len(names>25):
+            return chunk_names(names, notification_email, batch)
+        
+        else:
+            emails, errors = blackbaud(names)
+
+            if emails:
+                tagging_failed = drip(emails)
+            
+            message, cc = generate_message(emails, errors, notification_email, tagging_failed)
+            send_email(notification_email, cc, message, batch)
+
     except Exception as e:
         message = e
         cc = 'andrew@glacier.org'
         notification_email = notification_email if notification_email else 'andrew@glacier.org'
         batch = batch if batch else ''
         send_email(notification_email, cc, message, batch)
-
-    if emails:
-        tagging_failed = drip(emails)
-    
-    message, cc = generate_message(emails, errors, notification_email, tagging_failed)
-    send_email(notification_email, cc, message, batch)
 
     return message
